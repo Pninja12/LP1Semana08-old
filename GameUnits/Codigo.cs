85af90972c3592584a0192729bd650caea44742a
Paulo Silva <a22206205@alunos.ulht.pt>
2023-05-05 15:05:06 +0100

    Exercício 6 Concluído

---
+++ GameUnits/Codigo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GameUnits
+{
+    public abstract class Unit
+    {
+        
+            public abstract float Cost { get; }
+            public int movement;
+            public virtual int Health { get; set;}
+            
+            public Unit (int movement , int health ) 
+            {
+                this.movement = movement ;
+                Health = health ;
+            }
+
+        public void Move()
+        {
+            Console.WriteLine($"Andou {movement} casas");
+        }
+
+        public void ToString()
+        {
+            return base.GetType().name + ": HP=" + HP + " Cost=" + Cost;
+        }
+    }
+
+    public class MilitaryUnit : Unit    
+    {
+        public int AttackPower { get; }
+        public int XP { get ; private set; }
+        public void Attack ( Unit u ){}
+
+        public override float Cost => AttackPower + XP;
+
+        public MilitaryUnit(int health, int movement, int attackPower) : base(movement, health)
+        {
+            AttackPower = attackPower;
+            XP = 0;
+        }
+
+        public override int Health => XP + base.Health;
+
+        public void Attack()
+        {
+            XP++;
+            Health--;
+        }
+
+        public void ToString()
+        {
+            return base.GetType().name + ": HP=" + HP + " Cost=" + Cost + " AP=" + AttackPower + " XP=" + XP;
+        }
+    }
+
+    public class SettlerUnit : Unit
+    {
+        public override float Cost => 5;
+        public SettlerUnit(int health, int movement) : base(1, 2)
+        {
+        }
+    }
+}
--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -1,66 +1,46 @@
 ﻿using System;
+using System.Numerics;
 
 namespace GameUnits
 {
-    public abstract class Unit
+    class Program
     {
-        
-            public abstract float Cost { get; }
-            public int movement;
-            public virtual int Health { get; set;}
-            
-            public Unit (int movement , int health ) 
+        private static void Main()
+        {
+            // Criar array com três unidades
+            Unit[] units = new Unit[]
             {
-                this.movement = movement ;
-                Health = health ;
+                new MilitaryUnit(3, 10, 5),
+                new MilitaryUnit(4, 5, 3),
+                new SettlerUnit(),
+                new DiplomatUnit(),
+                new DiplomatUnit()
+            };
+
+            // Unidade 0 ataca unidade 1
+            (units[0] as MilitaryUnit).Attack(units[1]);
+            // Unidade 1 ataca unidade 2
+            (units[1] as MilitaryUnit).Attack(units[2]);
+            // Unidade 3 faz acordo comercial com unidade 4
+            (units[3] as DiplomatUnit).MakeAgreement(units[4]);
+            // Unidade 4 faz acordo comercial com unidade 2
+            (units[4] as DiplomatUnit).MakeAgreement(units[2]);
+
+            // "Imprimir" cada unidade
+            // chamando implicitamente o método ToString() de cada uma delas
+            foreach (Unit u in units)
+            {
+                Console.WriteLine(u);
             }
 
-        public void Move()
-        {
-            Console.WriteLine($"Andou {movement} casas");
-        }
-    }
+            // Output esperado:
+            //
+            // MilitaryUnit: HP=11 COST=6 XP=1 AP=5
+            // MilitaryUnit: HP=5 COST=4 XP=1 AP=3
+            // SettlerUnit: HP=1 COST=5
+            // DiplomatUnit: HP=1 COST=7.1 XP=3
+            // DiplomatUnit: HP=1 COST=7.1 XP=2
 
-    public class MilitaryUnit : Unit    
-    {
-        public int AttackPower { get; }
-        public int XP { get ; private set; }
-        public void Attack ( Unit u ){}
-
-        public override float Cost => AttackPower + XP;
-
-        public MilitaryUnit(int health, int movement, int attackPower) : base(movement, health)
-        {
-            AttackPower = attackPower;
-            XP = 0;
-        }
-
-        public override int Health => XP + base.Health;
-
-        public void Attack()
-        {
-            XP++;
-            Health--;
-        }
-    }
-
-    public class SettlerUnit : Unit
-    {
-        public override float Cost => 5;
-        public SettlerUnit(int health, int movement) : base(movement, health)
-        {
-            health = 2;
-            movement = 1;
-        }
-    }
-class Program
-    {
-        static void Main(string[] args)
-        {
-            MilitaryUnit mu = new MilitaryUnit(10, 2, 5);
-            MilitaryUnit mumove = new MilitaryUnit(23,4,60);
-            Console.WriteLine($"Health: {mu.Health}\nCost: {mu.Cost}");
-            Console.WriteLine($"Move: {mu.movement}");
         }
     }
 }
